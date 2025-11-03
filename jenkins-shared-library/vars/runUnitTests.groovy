3a87b02c06a742bde9dbb750f1c2151da2f875a2
MarwanTarek <167176241+marwantarek11@users.noreply.github.com>
2025-11-03 03:54:39 +0200

    Update runUnitTests.groovy

--- jenkins-shared-library/vars/runUnitTests.groovy
+++ jenkins-shared-library/vars/runUnitTests.groovy
@@ -3,7 +3,10 @@ def call() {
         set -e
         echo "🔹 Installing dependencies globally (bypassing PEP 668)..."
         pip install --break-system-packages --upgrade pip
-        pip install --break-system-packages -r requirements.txt
+        pip install --break-system-packages -r requirements.txt pytest
+
+        # Add user bin path so pytest and flask can be found
+        export PATH=$PATH:/var/lib/jenkins/.local/bin
 
         echo "🔹 Running tests..."
         pytest --junitxml=test-results.xml
