e801d2d03f92609df5547e23f12f6dfd76aede92
MarwanTarek <167176241+marwantarek11@users.noreply.github.com>
2025-11-03 03:47:46 +0200

    Update runUnitTests.groovy

--- jenkins-shared-library/vars/runUnitTests.groovy
+++ jenkins-shared-library/vars/runUnitTests.groovy
@@ -1,18 +1,11 @@
 def call() {
     sh '''
-        # Ensure Python & pip exist
-        if ! command -v python3 >/dev/null 2>&1; then
-            apt update && apt install -y python3 python3-pip
-        fi
+        apt update && apt install -y python3 python3-venv python3-pip
 
-        # Use user install directory to avoid permission errors
-        pip3 install --user --upgrade pip
-        pip3 install --user -r requirements.txt
+        python3 -m venv venv
+        . venv/bin/activate
 
-        # Add local bin path to PATH
-        export PATH="$HOME/.local/bin:$PATH"
-
-        # Run pytest
-        python3 -m pytest --junitxml=test-results.xml
+        pip install -r requirements.txt
+        pytest --junitxml=test-results.xml
     '''
 }
