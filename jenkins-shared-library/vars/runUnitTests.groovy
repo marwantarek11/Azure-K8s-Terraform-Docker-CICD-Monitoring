58443f81051c1d586d6136905feb5557aa6791c1
MarwanTarek <167176241+marwantarek11@users.noreply.github.com>
2025-11-03 03:35:14 +0200

    Update runUnitTests.groovy
    update to use docker agent

--- jenkins-shared-library/vars/runUnitTests.groovy
+++ jenkins-shared-library/vars/runUnitTests.groovy
@@ -1,21 +1,8 @@
 def call() {
-    sh '''
-        # Ensure Python and pip are installed
-        if ! command -v python3 >/dev/null 2>&1; then
-            echo "Python3 not found. Installing..."
-            sudo apt update && sudo apt install -y python3 python3-pip
-        fi
-
-        if ! command -v pip3 >/dev/null 2>&1; then
-            echo "pip3 not found. Installing..."
-            sudo apt update && sudo apt install -y python3-pip
-        fi
-
-        # Install dependencies
-        pip3 install --upgrade pip
-        pip3 install -r requirements.txt
-
-        # Run tests
-        python3 -m pytest --junitxml=test-results.xml
-    '''
+    docker.image('python:3.11').inside {
+        sh '''
+            pip install -r requirements.txt
+            python -m pytest --junitxml=test-results.xml
+        '''
+    }
 }
