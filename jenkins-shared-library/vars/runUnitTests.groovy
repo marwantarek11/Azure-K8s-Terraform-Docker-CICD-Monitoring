786646d7e3299337c8d8199ec7772742798bee25
MarwanTarek <167176241+marwantarek11@users.noreply.github.com>
2025-11-03 03:53:52 +0200

    Update runUnitTests.groovy

--- jenkins-shared-library/vars/runUnitTests.groovy
+++ jenkins-shared-library/vars/runUnitTests.groovy
@@ -1,9 +1,9 @@
 def call() {
     sh '''
         set -e
-        echo "🔹 Installing dependencies globally (CI-safe)..."
-        pip install --upgrade pip
-        pip install -r requirements.txt
+        echo "🔹 Installing dependencies globally (bypassing PEP 668)..."
+        pip install --break-system-packages --upgrade pip
+        pip install --break-system-packages -r requirements.txt
 
         echo "🔹 Running tests..."
         pytest --junitxml=test-results.xml
